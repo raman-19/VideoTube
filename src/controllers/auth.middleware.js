4dd0f0f9a5d0757fe3aeba492621b5829aefb2dc
Raman Tiwary <145109676+raman-19@users.noreply.github.com>
2026-04-21 15:05:59 +0530

    Fix token extraction and error handling in verifyJWT

--- src/controllers/auth.middleware.js
+++ src/controllers/auth.middleware.js
@@ -6,18 +6,19 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async(req,res,next)=>{
     try{
         const token = req.cookies?.accessToken || req.header
-      ("Authorization")?.replace("Bearer" , "")
+      ("Authorization")?.replace("Bearer " , "")
 
      if(!token){
         throw new ApiError(401,"Unauthorized request")
      }
 
      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-     await User.findById(decodedToken?._id)
+        
+     const user = await User.findById(decodedToken?._id)
      .select("-password  -refreshToken")
 
      if(!user){
-        // NEXT _VIDEO :DISCUSS ABOUT FRONTEND
+        
         throw new ApiError(401, "User not found ")
      }
      req.user = user;
@@ -25,7 +26,7 @@ export const verifyJWT = asyncHandler(async(req,res,next)=>{
 
 
     }catch(error){
-        throw new ApiError(404),error?.message || "Invalid access token"
+        throw new ApiError(401),error?.message || "Invalid access token"
 
     }
 })
